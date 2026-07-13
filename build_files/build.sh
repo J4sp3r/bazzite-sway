cfb5d0963ebfb12c3a537972f4273087c017cdb2
J4sp3r <J4sp3r>
2026-07-13 16:40:39 +0200

    Adds first version

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,23 +5,29 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
+### Install Sway and a minimal Wayland desktop stack on top of Bazzite.
+# Bazzite ships with KDE (or GNOME); we add Sway as an additional session
+# selectable from the display manager (SDDM).
+dnf5 install -y \
+    sway \
+    swaybg \
+    swayidle \
+    swaylock \
+    waybar \
+    foot \
+    wofi \
+    mako \
+    grim \
+    slurp \
+    wl-clipboard \
+    brightnessctl \
+    playerctl \
+    polkit-gnome \
+    xdg-desktop-portal-wlr \
+    NetworkManager-tui
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# Make sure the Sway wayland session is discoverable by display managers.
+# The sway package already installs /usr/share/wayland-sessions/sway.desktop,
+# but we enable a couple of user-facing services here.
 
 systemctl enable podman.socket
